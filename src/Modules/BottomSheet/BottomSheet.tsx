494e34cf75a6683d178e532af63ef74c75b9df89
Isaac Obeng <mrimoempire@gmail.com>
2023-07-11 12:42:00 +0000

    update bottomsheet

--- src/Modules/BottomSheet/BottomSheet.tsx
+++ src/Modules/BottomSheet/BottomSheet.tsx
@@ -3,42 +3,69 @@ import {
   Animated,
   Dimensions,
   Modal,
+  Pressable,
+  ScrollView,
+  StyleProp,
   StyleSheet,
   Text,
   TouchableOpacity,
   TouchableWithoutFeedback,
   View,
+  ViewStyle,
 } from "react-native";
 import { PanGestureHandler } from "react-native-gesture-handler";
 import AntDesign from "react-native-vector-icons/AntDesign";
 
 interface Props {
+  // STATES
   show?: boolean;
+  enableBackdropDismiss?: boolean;
+  // ACTIONS
   setShow?: (value: boolean | ((prevVar: boolean) => boolean)) => void;
   onDismiss?: any;
+  // STYLES
   bottomSheetheight?: number;
+  headerContainer?: StyleProp<ViewStyle>;
+  contentContainer?: StyleProp<ViewStyle>;
+  // CHILDEREN
   children?: any;
 }
 
 const height = Dimensions.get("window").height * 0.5;
 const BottomSheet = ({
+  enableBackdropDismiss = true,
   show,
   setShow,
   onDismiss,
   bottomSheetheight = height,
+  headerContainer = {
+    height: 44,
+    backgroundColor: "#ffff",
+  },
+  contentContainer = {
+    backgroundColor: "#ffffff",
+    paddingHorizontal: 10,
+    height: "100%",
+    paddingBottom: 20,
+  },
   children,
 }: Props) => {
   const [open, setOpen] = useState(show);
   const width = Dimensions.get("window").width;
 
+  let contentStyles = {
+    // @ts-ignore
+    ...contentContainer,
+    paddingHorizontal: 10,
+    height: "100%",
+    paddingBottom: 20,
+  };
   const buttonRef = useRef(new Animated.Value(-bottomSheetheight)).current;
-
   const onGesture = (event: any) => {
     if (event.nativeEvent.translationY > 0) {
       buttonRef.setValue(-event.nativeEvent.translationY);
     }
   };
-
   const onGestureEnd = (event: any) => {
     if (event.nativeEvent.translationY > bottomSheetheight / 2) {
       onDismiss();
@@ -72,6 +99,10 @@ const BottomSheet = ({
 
   return (
     <>
+      <Pressable
+        onPress={enableBackdropDismiss ? onDismiss : undefined}
+        style={styles.backDrop}
+      />
       <Animated.View
         style={[
           styles.container,
@@ -82,37 +113,29 @@ const BottomSheet = ({
         ]}
       >
         <PanGestureHandler onGestureEvent={onGesture} onEnded={onGestureEnd}>
-          <View style={[styles.header, { position: "relative" }]}>
+          <View style={[headerContainer, { position: "relative" }]}>
             <View
               style={{
-                width: 50,
-                height: 5,
+                width: 60,
+                height: 7,
+                borderRadius: 10,
                 position: "absolute",
                 top: 8,
+                left: (width - 60) / 2,
                 zIndex: 10,
-                left: (width - 40) / 2,
-                backgroundColor: "red",
-                borderRadius: 5,
+                backgroundColor: "#ccc",
               }}
             />
             <View
-              style={{ position: "absolute", right: 10, top: 5, zIndex: 10 }}
+              style={{ position: "absolute", right: 15, top: 10, zIndex: 10 }}
             >
               <TouchableOpacity onPress={onDismiss} activeOpacity={1}>
-                <AntDesign name="closecircle" size={24} color="black" />
+                <AntDesign name="closecircle" size={26} color="black" />
               </TouchableOpacity>
             </View>
           </View>
         </PanGestureHandler>
-        <View
-          style={{
-            padding: 20,
-            backgroundColor: "red",
-            height: "100%",
-          }}
-        >
-          {children}
-        </View>
+        <ScrollView style={contentStyles}>{children}</ScrollView>
       </Animated.View>
     </>
   );
@@ -126,15 +149,14 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     zIndex: 100,
-    backgroundColor: "#ffffff",
-    borderTopRightRadius: 15,
-    borderTopLeftRadius: 15,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
     overflow: "hidden",
   },
-  header: {
-    height: 33,
-    backgroundColor: "#ccc",
-    justifyContent: "center",
-    elevation: 2,
+  backDrop: {
+    ...StyleSheet.absoluteFillObject,
+    zIndex: 80,
+    backgroundColor: "rgba(0,0,0, 0.12)",
   },
 });
