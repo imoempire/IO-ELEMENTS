698fa3c178a20b1a93536d6c96c0cf2da63c234c
Isaac Obeng <mrimoempire@gmail.com>
2023-08-04 21:54:22 +0000

    update

--- src/Modules/Provider/Provider.tsx
+++ src/Modules/Provider/Provider.tsx
@@ -5,14 +5,14 @@ import { Keyboard, TouchableWithoutFeedback, View } from "react-native";
 // @ts-ignore
 const MyContext = React?.createContext();
 
+const DismissKeyboard = ({ children }: { children: any }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <View style={{ flex: 1 }}>{children}</View>
+  </TouchableWithoutFeedback>
+);
+
 const IoElementsProvider = ({ children }: { children: any }) => {
   const [sharedState, setSharedState] = useState<any>(/* initial state */);
-
-  const DismissKeyboard = ({ children }: { children: any }) => (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={{ flex: 1 }}>{children}</View>
-    </TouchableWithoutFeedback>
-  );
 
   return (
     <MyContext.Provider value={{ sharedState, setSharedState }}>
